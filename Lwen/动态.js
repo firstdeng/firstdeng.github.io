555f8197d1b87f38d7b692768a0a6fb5b9ea03b6
firstdeng <33589620+firstdeng@users.noreply.github.com>
2019-04-28 11:31:24 +0800

    Add files via upload

---
+++ Lwen/动态.js
@@ -0,0 +1,25 @@
+var adImages = new Array("image/15.jpg",
+    "image/31.jpg",
+    "image/23.jpg",
+    "image/30.jpg",
+    "image/33.jpg");
+
+
+function rotate() {
+    thisAd++;
+    if (thisAd == adImages.length) {
+        thisAd = 0;
+    }
+    document.getElementById("adBanner").src = adImages[thisAd];
+
+    setTimeout("rotate()", 1 * 1000);
+}
+
+
+function choosePic() {
+    thisAd = Math.floor((Math.random() * adImages.length));
+    document.getElementById("adBanner").src = adImages[thisAd];
+
+    rotate();
+
+}
