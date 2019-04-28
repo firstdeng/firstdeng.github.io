a67f984a235132fc3dda69e9ce7b263f70158b45
firstdeng <33589620+firstdeng@users.noreply.github.com>
2019-04-28 11:17:45 +0800

    Add files via upload

--- lunwen/动态.js
+++ lunwen/动态.js
@@ -1,3 +1,28 @@
+<<<<<<< HEAD
+var adImages = new Array(
+    "image/31.jpg",
+    "image/23.jpg",
+    "image/33.jpg",
+    "image/15.jpg", );
+
+
+function rotate() {
+    thisAd++;
+    if (thisAd == adImages.length) {
+        thisAd = 0;
+    }
+    document.getElementById("adBanner").src = adImages[thisAd];
+
+    setTimeout("rotate()", 1 * 1000);
+}
+
+
+function choosePic() {
+    thisAd = Math.floor((Math.random() * adImages.length));
+    document.getElementById("adBanner").src = adImages[thisAd];
+
+    rotate();
+=======
 
 
 var adImages = new Array("image/15.jpg",
@@ -23,4 +48,5 @@ function choosePic() {
 	document.getElementById("adBanner").src = adImages[thisAd];
 	
 	rotate();
+>>>>>>> 90308fed96fd6dc0c6fa34f36db93a17dbaee935
 }
